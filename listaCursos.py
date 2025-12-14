fe75de327b49c572cbe3b3b82452278e54a46b5d
duridev <duridev@outlook.com>
2025-12-14 01:08:32 -0300

    creacion de lista de cursos

---
+++ listaCursos.py
@@ -0,0 +1,45 @@
+cursos = [
+    {
+        id: 1,
+        "nombre": "JavaScript Moderno Guía Definitiva Construye +20 Proyectos",
+        "categoria": "Javascript",
+        "precio": 9900
+    },
+    {
+        id: 2,
+        "nombre": "React y TypeScript - La Guía Completa Creando +10 Proyectos",
+        "categoria": "React",
+        "precio": 9900
+    },
+    {
+        id: 3,
+        "nombre": "Vue.js 3 - La Guía Completa - Composition Pinia MEVN 10 Apps",
+        "categoria": "Vue",
+        "precio": 9900
+    },
+    {
+        id: 4,
+        "nombre": "Full Stack Node.js React TS NestJS Next.js Creando Proyectos",
+        "categoria": "Fullstack",
+        "precio": 9900
+    },
+    {
+        id: 5,
+        "nombre": "React Native - Crea aplicaciones para Android y iOS c/ React",
+        "categoria": "Movil",
+        "precio": 9900
+    },
+]
+
+
+
+
+
+
+
+
+print("Bienvenido a tu Ecommerce")
+
+print(
+    "\n1) Ver "
+)
